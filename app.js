298e9934fce43984cd13fd8a241de27a486a9bfc
Neilson <hc3wong@uwaterloo.ca>
2017-01-11 10:37:50 -0500

    migrated all functions into lastfm

--- app.js
+++ app.js
@@ -3,46 +3,8 @@
 var async = require("async");
 var fs = require("fs");
 var lastfm = require("./lastfm");
-var fixLastFmData = require("./fixLastFmData");
 
-function getDownloadedDatabases(callback){
-    let testFolder = "database";
-    fs.readdir(testFolder, (err, files) => {
-        if (callback){
-            callback(files);
-        }
-    });
-}
 
-function parseDownloadedDatabase(file, callback){
-    fs.readFile(file, function(err, data){
-        let db = JSON.parse(data).weeklytrackchart;
-        fixLastFmData.aggregateWeeklyTracks(db, callback);
-    });
-}
-
-function generateFixedLastfmDb() {
-    let db3 = {};
-
-    getDownloadedDatabases(function(files){
-        let i, song;
-        let topsong = {};
-        async.each(files, function(file, done){
-            parseDownloadedDatabase("database/"+file, function(week){
-                if (week !== null){
-                    db3[week.from] = week;
-                }
-                done();
-            });
-        },
-        function done(err){
-            var songs = fixLastFmData.buildSongBook(db3);
-
-            console.log("writing song files");
-            fs.writeFileSync("songs.json", JSON.stringify(songs.all, null, 2));
-            fs.writeFileSync("playable.json", JSON.stringify(songs.playable, null, 2));
-        });
-    });
-}
-
-lastfm.updateDbFiles(generateFixedLastfmDb);
+lastfm.updateData(function(success){
+    console.log(success ? "yay" : "nay");
+});
--- lastfm.js
+++ lastfm.js
@@ -1,12 +1,16 @@
 "use strict";
 
-var request = require("request");
-var async = require("async");
-var fs = require("fs");
+let request = require("request");
+let async = require("async");
+let fs = require("fs");
+
+let fixLastFmData = require("./fixLastFmData");
 
 function Lastfm(){}
+
 Lastfm.apiKey = "200ef837557217e186dd2eed9a6075b8";
 Lastfm.user = "mahourin";
+Lastfm.beginningOfTime = 1318052895;
 
 Lastfm.grabWeeks = function (callback) {
     request.post("http://ws.audioscrobbler.com/2.0/", {
@@ -23,7 +27,7 @@ Lastfm.grabWeeks = function (callback) {
         try {
             var data = JSON.parse(body);
             var weeks = data.weeklychartlist.chart.filter(function (obj) {
-                return (obj.to > 1318052895);
+                return (obj.to > Lastfm.beginningOfTime);
             }).map(function (obj) {
                 return { to: obj.to, from: obj.from };
             });
@@ -52,7 +56,7 @@ Lastfm.getTracks = function (to, from, callback) {
             console.log(err);
         }
         try {
-            callback(body);
+            return callback(body);
         }
         catch (e) {
             console.log("get tracks error");
@@ -61,10 +65,19 @@ Lastfm.getTracks = function (to, from, callback) {
     });
 };
 
+Lastfm.generateDbFiles = function downloadDbFiles(callback){
+    return genDb(Lastfm.beginningOfTime, callback);
+};
 
 Lastfm.updateDbFiles = function updateDbFiles(callback){
     //don't need to update things from more than a month ago
+    //2629743000 is one month
+
     let cutoff = new Date().getTime() - 2629743000;
+    return genDb(cutoff, callback);
+}
+
+let genDb = function genDb(cutoff, callback){
     Lastfm.grabWeeks(function(weeks){
         async.eachSeries(weeks, function Iter(item, next){
             var fromdate =  new Date(1000 * item.from);
@@ -81,17 +94,87 @@ Lastfm.updateDbFiles = function updateDbFiles(callback){
             }
             else {
                 console.log("skipping " + fromdate);
-                next();
+                return next();
             }
         }, function done(){
             //all done
             //fs.writeFileSync("database2.json", JSON.stringify(db2));
             console.log("up to date");
             if (callback){
-                callback();
+                return callback();
             }
         });    
     });
+};
+
+Lastfm.generateFixedLastfmDb = function generateFixedLastfmDb(callback) {
+    let getDownloadedDatabases = function(callback){
+        let testFolder = "database";
+        fs.readdir(testFolder, (err, files) => {
+            if (callback){
+                return callback(files);
+            }
+        });
+    }
+
+    let parseDownloadedDatabase = function(file, callback){
+        fs.readFile(file, function(err, data){
+            let db = JSON.parse(data).weeklytrackchart;
+            return fixLastFmData.aggregateWeeklyTracks(db, callback);
+        });
+    }
+
+    let db3 = {};
+
+    getDownloadedDatabases(function(files){
+        let i, song;
+        let topsong = {};
+        async.each(files, function(file, done){
+            parseDownloadedDatabase("database/"+file, function(week){
+                if (week !== null){
+                    db3[week.from] = week;
+                }
+                return done();
+            });
+        },
+        function done(err){
+            var songs = fixLastFmData.buildSongBook(db3);
+
+            console.log("writing song files");
+            fs.writeFileSync("songs.json", JSON.stringify(songs.all, null, 2));
+            fs.writeFileSync("playable.json", JSON.stringify(songs.playable, null, 2));
+
+            return callback(true);
+        });
+    });
 }
+
+Lastfm.updateData = function updateData(callback) {
+    let dbPath = "database";
+    let fresh = false;
+
+    async.series([
+        function ensureDbFolderExists(next) {
+            fs.access(dbPath, function(err) {
+                if (err){
+                    //cannot hit db path
+                    //lets make it
+                    fresh = true;
+                    return fs.mkdir(dbPath, next);
+                }
+                else {
+                    return next();
+                }
+            });
+        },
+        function getDataFromLastfm(next){
+            return fresh ? Lastfm.generateDbFiles(next) : Lastfm.updateDbFiles(next);
+        }
+    ],
+    function fixDownloadedData(next){
+        //synchronous operation
+        return Lastfm.generateFixedLastfmDb(callback);
+    });
+};
 
 module.exports = Lastfm;
